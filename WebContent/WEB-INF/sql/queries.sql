4dc759f79ce066e084d283b2e26da5cdd8ca193f
Binu Paulmoni <binu.mymailbox@gmail.com>
2011-03-10 11:59:04 +0530

    Duplicate VendorMaterial Creation - Fix (Not all cases are tested though)

--- WebContent/WEB-INF/sql/queries.sql
+++ WebContent/WEB-INF/sql/queries.sql
@@ -3,8 +3,13 @@ select * from purchase_orders order by purchase_order_id desc;
 select * from vendor_materials where vendor_id = 100;
 select * from materials;
 select * from vendors;
+select * from ranks;
 
 update vendor_materials set price_per_qty = 10 where vendor_material_id = 1;
 select * from users;
 select * from addresses;-- where address_id = 130;
+select * from vendor_materials where vendor_id = 32;
+
+delete from vendor_materials where vendor_id = 32;
+
 
